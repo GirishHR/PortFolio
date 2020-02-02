105eee796d3e451ee8fdd74d03d543ae6e08115f
GirishHR <girishr.tiptur@gmail.com>
2020-02-03 02:10:56 +0530

    updated responsiveness

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit
+{
+  fileUrl;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit()
+  {
+
   }
 
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,7 +8,9 @@ import { Chart } from 'chart.js';
 })
 export class PortfolioComponent implements OnInit
 {
-  chart: any;
+  sslcchart: any;
+  pucchart: any;
+  bechart: any;
   constructor() { }
 
   ngOnInit()
@@ -22,24 +24,80 @@ export class PortfolioComponent implements OnInit
         const ctx = chart.chart.ctx;
         ctx.restore();
         const fontSize = (height / 114).toFixed(2);
-        ctx.font = fontSize + 'em sans-serif';
-        ctx.textBaseline = 'middle';
-
-        const text = "90%";
+        ctx.font = fontSize + "em sans-serif";
+        ctx.textBaseline = "middle";
+        const text = chart.config.options.elements.center.text;
         const textX = Math.round((width - ctx.measureText(text).width) / 2);
         const textY = height / 2;
-
         ctx.fillText(text, textX, textY);
         ctx.save();
       }
     });
-    this.chart = new Chart('canvas', {
+    this.sslcchart = new Chart('sslccanvas', {
       type: 'doughnut',
       data: {
         labels: ['Data1', 'Data2'],
         datasets: [
           {
             data: [90, 10],
+            backgroundColor: ['#0072c6', 'whitesmoke'],
+            fill: false
+          },
+        ]
+      },
+      options: {
+        animation: {
+          animateScale: true
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: false
+        },
+        elements: {
+          center: {
+            text: '90%'  // set as you wish
+          }
+        },
+      }
+    });
+    this.pucchart = new Chart('puccanvas', {
+      type: 'doughnut',
+      data: {
+        labels: ['Data1', 'Data2'],
+        datasets: [
+          {
+            data: [87, 13],
+            backgroundColor: ['#4099ff', 'whitesmoke'],
+            fill: false
+          },
+        ]
+      },
+      options: {
+        animation: {
+          animateScale: true
+        },
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: false
+        },
+        elements: {
+          center: {
+            text: '87%'  // set as you wish
+          }
+        },
+      }
+    });
+    this.bechart = new Chart('becanvas', {
+      type: 'doughnut',
+      data: {
+        labels: ['Data1', 'Data2'],
+        datasets: [
+          {
+            data: [91.7, 8.3],
             backgroundColor: ['#0077b5', 'whitesmoke'],
             fill: false
           },
@@ -54,7 +112,12 @@ export class PortfolioComponent implements OnInit
         },
         tooltips: {
           enabled: false
-        }
+        },
+        elements: {
+          center: {
+            text: '9.17'  // set as you wish
+          }
+        },
       }
     });
   }
